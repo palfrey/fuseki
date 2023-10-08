db3a31f7e08b0a621525903b8551ab4777eb3302
Tom Parker-Shemilt <palfrey@tevp.net>
2023-10-08 13:01:08 +0100

    Redraw all the stones so we get captures

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::time::Duration;
 
-use gtp::{controller::Engine, Response};
 use gtp::Command;
+use gtp::{controller::Engine, Response};
 use libremarkable::{
-    appctx, cgmath::{self, Point2},
+    appctx,
+    cgmath::{self, Point2},
     framebuffer::{
         common::{color, display_temp, dither_mode, mxcfb_rect, waveform_mode},
         core::Framebuffer,
         FramebufferDraw, FramebufferRefresh,
-    }, input::{InputEvent, MultitouchEvent},
+    },
+    input::{InputEvent, MultitouchEvent},
 };
 use log::info;
 
@@ -24,10 +26,8 @@ const SPARE_HEIGHT: u16 =
     (libremarkable::dimensions::DISPLAYHEIGHT - (SQUARE_SIZE * SQUARE_COUNT as u16)) / 2;
 const BORDER_WIDTH: u32 = 10;
 
-fn loop_update_topbar(app: &mut appctx::ApplicationContext<'_>, millis: u64) {}
-
 fn draw_piece(fb: &mut Framebuffer, x: u8, y: u8, white: bool) {
-    info!("draw_piece: {x} {y} {white}");
+    // info!("draw_piece: {x} {y} {white}");
     let point = cgmath::Point2 {
         x: (SPARE_WIDTH + (SQUARE_SIZE * x as u16)) as i32,
         y: (SPARE_HEIGHT + (SQUARE_SIZE * y as u16)) as i32,
@@ -44,9 +44,7 @@ fn get_response(ctrl: &mut Engine) -> Response {
             Ok(resp) => {
                 return resp;
             }
-            Err(gtp::controller::Error::PollAgain) => {
-                continue
-            }
+            Err(gtp::controller::Error::PollAgain) => continue,
             Err(err) => {
                 panic!("Other error {err:?}")
             }
@@ -54,27 +52,21 @@ fn get_response(ctrl: &mut Engine) -> Response {
     }
 }
 
-fn do_machine_move(ctrl: &mut Engine, fb: &mut Framebuffer) {
-    ctrl.send(Command::new_with_args("genmove", |e| e.
-    s("black")));
+fn do_machine_move(ctrl: &mut Engine) {
+    ctrl.send(Command::new_with_args("genmove", |e| e.s("black")));
+    info!("waiting for machine response");
     let resp = get_response(ctrl);
     info!("machine: {}", resp.text());
-    let ev = resp.entities(|ep| ep.vertex()).unwrap();
-    match ev.first().unwrap() {
-        gtp::Entity::Vertex((x,y)) => {
-            draw_piece(fb, *x as u8, *y as u8, false);        
-        }
-        _ => {}
-    }
 }
 
-fn do_human_move(ctrl: &mut Engine, fb: &mut Framebuffer, pos: Point2<u8>) {
-    let cmd = Command::new_with_args("play", |e| e.s("white").v((pos.x as i32, pos.y as i32)).list());
+fn do_human_move(ctrl: &mut Engine, pos: Point2<u8>) {
+    let cmd = Command::new_with_args("play", |e| {
+        e.s("white").v((pos.x as i32, pos.y as i32)).list()
+    });
     info!("human: {}", cmd.to_string());
     ctrl.send(cmd);
     let resp = get_response(ctrl);
     info!("human resp: {}", resp.text());
-    draw_piece(fb, pos.x, pos.y, true);
 }
 
 fn refresh(fb: &mut Framebuffer) {
@@ -94,12 +86,8 @@ fn refresh(fb: &mut Framebuffer) {
     );
 }
 
-fn main() {
-    env_logger::init();
-    let mut app: appctx::ApplicationContext<'_> = appctx::ApplicationContext::default();
-    app.clear(true);
-
-    let fb = app.get_framebuffer_ref();
+fn draw_grid(fb: &mut Framebuffer) {
+    fb.clear();
 
     for y in 0..SQUARE_COUNT {
         for x in 0..SQUARE_COUNT {
@@ -117,22 +105,62 @@ fn main() {
             );
         }
     }
+}
 
-    // Get a &mut to the framebuffer object, exposing many convenience functions
-    let appref = app.upgrade_ref();
-    let clock_thread = std::thread::spawn(move || {
-        loop_update_topbar(appref, 30 * 1000);
+fn list_stones(ctrl: &mut Engine, color: &str) -> Vec<gtp::Entity> {
+    let cmd = Command::new_with_args("list_stones", |e| e.s(color));
+    // info!("list_stones: {}", cmd.to_string());
+    ctrl.send(cmd);
+    let resp = get_response(ctrl);
+    // info!("list_stones resp: {}", resp.text());
+    let ev = resp.entities(|ep| {
+        let mut ret = ep;
+        while !ret.is_eof() {
+            ret = ret.vertex();
+        }
+        ret
     });
+    return ev.unwrap();
+}
+
+fn draw_stones(fb: &mut Framebuffer, ev: Vec<gtp::Entity>, white: bool) {
+    for entity in ev {
+        match entity {
+            gtp::Entity::Vertex((x, y)) => {
+                draw_piece(fb, x as u8, y as u8, white);
+            }
+            _ => {}
+        }
+    }
+}
+
+fn redraw_stones(ctrl: &mut Engine, fb: &mut Framebuffer) {
+    draw_grid(fb);
+    let white_stones = list_stones(ctrl, "white");
+    draw_stones(fb, white_stones, true);
+    let black_stones = list_stones(ctrl, "black");
+    draw_stones(fb, black_stones, false);
+    refresh(fb);
+}
+
+fn main() {
+    env_logger::init();
+    let mut app: appctx::ApplicationContext<'_> = appctx::ApplicationContext::default();
+
+    let fb = app.get_framebuffer_ref();
+    draw_grid(fb);
 
     info!("Starting GnuGo");
     let mut ctrl = Engine::new("./gnugo", &["--mode", "gtp"]);
     assert!(ctrl.start().is_ok());
 
-    ctrl.send(Command::new_with_args("boardsize", |e| e.i(BOARD_SIZE as u32)));
+    ctrl.send(Command::new_with_args("boardsize", |e| {
+        e.i(BOARD_SIZE as u32)
+    }));
     ctrl.wait_response(Duration::from_millis(500)).unwrap();
 
-    do_machine_move(&mut ctrl, fb);
-    refresh(fb);
+    do_machine_move(&mut ctrl);
+    redraw_stones(&mut ctrl, fb);
     info!("Init complete. Beginning event dispatch...");
 
     // Blocking call to process events from digitizer + touchscreen + physical buttons
@@ -142,27 +170,30 @@ fn main() {
             info!("event: {ev:?}");
         }
     });
-    clock_thread.join().unwrap();
 }
 
 fn nearest_spot(x: u16, y: u16) -> Point2<u8> {
     Point2 {
-        x: (((x-SPARE_WIDTH) as f32)/(SQUARE_SIZE as f32)).round() as u8,
-        y: (((y-SPARE_HEIGHT) as f32)/(SQUARE_SIZE as f32)).round() as u8,
+        x: (((x - SPARE_WIDTH) as f32) / (SQUARE_SIZE as f32)).round() as u8,
+        y: (((y - SPARE_HEIGHT) as f32) / (SQUARE_SIZE as f32)).round() as u8,
     }
 }
 
-fn on_multitouch_event(ctx: &mut appctx::ApplicationContext<'_>, event: MultitouchEvent, ctrl: &mut Engine) {
+fn on_multitouch_event(
+    ctx: &mut appctx::ApplicationContext<'_>,
+    event: MultitouchEvent,
+    ctrl: &mut Engine,
+) {
     match event {
         MultitouchEvent::Press { finger } => {
             let fb = ctx.get_framebuffer_ref();
             let point = nearest_spot(finger.pos.x, finger.pos.y);
             let pos = finger.pos;
             info!("Drawing: {point:?} for {pos:?}");
-            do_human_move(ctrl, fb, point);
-            refresh(fb);
-            do_machine_move(ctrl, fb);
-            refresh(fb);
+            do_human_move(ctrl, point);
+            redraw_stones(ctrl, fb);
+            do_machine_move(ctrl);
+            redraw_stones(ctrl, fb);
         }
         _ => {}
     }
